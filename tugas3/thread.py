7dabbe8b4efeacc18771a9cc8c9988809f543eaa
belutooth <lostman.fikri@gmail.com>
2020-03-09 12:11:36 +0700

    screen capt

--- tugas3/thread.py
+++ tugas3/thread.py
@@ -4,10 +4,9 @@ import requests
 import datetime
 import os
 
-def download_gambar(url=None):
+def download_picture(url=None):
     if (url is None):
         return False
-
     ff = requests.get(url)
     tipe = dict()
     tipe['image/png']='png'
@@ -17,24 +16,26 @@ def download_gambar(url=None):
     content_type = ff.headers['Content-Type']
     logging.warning(content_type)
     if (content_type in list(tipe.keys())):
-        namafile = os.path.basename(url)
-        ekstensi = tipe[content_type]
-        logging.warning(f"writing {namafile}.{ekstensi}")
-        fp = open(f"{namafile}.{ekstensi}","wb")
+        filename = os.path.basename(url)
+        extension = tipe[content_type]
+        logging.warning(f"writing {filename}.{extension}")
+        fp = open(f"{filename}.{extension}","wb")
         fp.write(ff.content)
         fp.close()
     else:
         return False
 
+
 if __name__=='__main__':
-
-    multi_threads = []
-    url1 = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'
-    url2 = 'https://cdn.pixabay.com/photo/2018/11/14/20/50/hd-3816045_960_720.jpg'
-    g1 = threading.Thread(target=download_gambar, args=(url1,))
-    multi_threads.append(g1)
-    g2 = threading.Thread(target=download_gambar, args=(url2,))
-    multi_threads.append(g2)
-
-    for i in multi_threads:
-        i.start()
+    picture_list = [
+        'https://i.pinimg.com/474x/3f/df/a3/3fdfa3c237e228c38a0311e170be78a9--why-do-men-heavens.jpg',
+        'https://i.etsystatic.com/13298123/r/il/cd2108/2032802252/il_794xN.2032802252_jlui.jpg',
+        'https://i.pinimg.com/originals/c7/9b/a7/c79ba794d04613d1bd3ad54a8f4c8022.jpg'
+        
+    ]
+    
+threads = []
+for i in picture_list:
+    t = threading.Thread(target=download_picture,args=(i,))
+    threads.append(t)
+    t.start()
